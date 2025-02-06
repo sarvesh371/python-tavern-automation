5a04bb16b5aac87c04ece865b912d94599400902
sarvesh371 <sarveshsingh.03@gmail.com>
2025-02-06 18:59:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # python-tavern-automation
 Repo for learning tavern pytest
+
+## Doc reference
+
+- [Tavern](https://tavern.readthedocs.io/en/latest/)
+- [pytest](https://docs.pytest.org/en/7.1.x/)
+
+## Run tests
+
+```bash
+pytest test_sample_test.tavern.yaml
+```
+
---
+++ venv_setup.sh
@@ -0,0 +1,9 @@
+export PYTHONHTTPSVERIFY=0
+export PYTHON_VENV=venv
+rm -rf $PYTHON_VENV
+python3 -m pip install virtualenv
+python3 -m virtualenv $PYTHON_VENV
+source $PYTHON_VENV/bin/activate
+python3 -m pip install -U pip
+python3 -m pip install -U virtualenv
+python3 -m pip install --requirement requirements.txt
